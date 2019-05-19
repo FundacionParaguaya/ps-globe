7e7637c0b69f263040a7f0a62ffc2eea6e7b0971
Bryan Giger <bryan@gigerbyt.es>
2019-05-19 01:30:36 -0400

    use process.env for socket url

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = ({ port }) => {
   const [focus, setFocus] = useState(undefined)
   const socket = useMemo(
     () => {
-      const s = openSocket(`ps-globe-backend.us-east-2.elasticbeanstalk.com`)
+      const s = openSocket(process.env.SOCKET_URL)
       console.log(s)
       return s
     },
